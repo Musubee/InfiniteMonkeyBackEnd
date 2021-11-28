a0f1834a5e0a37eea5f589a621f1b7665b5c4ffe
Aramis Subee <subee@mit.edu>
2021-11-27 17:53:33 -1000

    Altered simulator's output to match database schema.

--- src/simulator.py
+++ src/simulator.py
@@ -1,50 +1,50 @@
-# The Simulator class will produce random charamaintain the current state of the simulation, and post to the database when a mistake is made (run completed)
+# The Simulator class will produce random characters, maintain the current state of the simulation, and post to the database when a mistake is made (run completed)
 
 
-# TODO:
-# 1. add datetime to next() (once schema is known)
 import random
 import trie
+from datetime import datetime
 from __future__ import annotations
 
 class Simulator:
-    def __init__(self, trie: dict[str, dict|float|bool], valid_elements: str|list[str]):
+    def __init__(self, trie: dict[str, dict|float|bool], valid_elements: str):
         self.trie = trie
         self.curr_node = trie
         self.curr_string = ''
         self.valid_elements = valid_elements
-        if type(self.valid_elements) == str:
-            self.using_chars = True
 
         self.done = False
 
-    def next(self) -> tuple[str, int] | tuple[None, None]:
-        # Gets a random character and returns the generated character and updated completion percentage
+    def next(self) -> tuple[str, float, str, str, bool] | None:
+        # Generates a random character and updates internal state of simulator
+        # Returns information according to the database schema including:
+        # 1. Current generated string (character count derived from this string)
+        # 2. Highest match percentage
+        # 3. Corresponding work
+        # 4. Datetime (UTC)
+        # Also returns a flag denoting whether a mismatch has occurred, and therefore
+        # the calling function should post the returned result to the database
         if self.done:
-            return (None, None)
+            return None
 
         next_element = random.choice(self.valid_elements)
+
+        # Found a match
         if next_element in self.curr_node:
             self.curr_node = self.curr_node[next_element]
-            if self.using_chars:
-                self.curr_string += next_element
-            else:
-                self.curr_string += next_element + ' '
+            self.curr_string += next_element
+            if self.curr_node['is_done']:
+                self.done = True
+            return self.curr_string, self.curr_node['max_percentage'], self.curr_node['work_title'],
+                    str(datetime.now()), self.done
         else:
+            # Return info to post to database
+            generated_string = self.curr_string
+            match_percentage = self.curr_node['max_percentage']
+            work = self.curr_node['work_title']
+            time = str(datetime.now())
             self.curr_node = self.trie
 
             self.curr_string = ''
-
-        if self.curr_node['is_done']:
-            self.done = True
-
-        return (next_element, self.curr_node['max_percentage'])
-
-    def get_current_percentage(self) -> int:
-        return self.curr_node['max_percentage']
-
-    def get_current_string(self) -> str:
-        return self.curr_string
-    
-    def get_matched_works(self) -> List[str]:
-        return self.curr_node['work_ids']
+            
+            return generated_string, match_percentage, work, time, True
